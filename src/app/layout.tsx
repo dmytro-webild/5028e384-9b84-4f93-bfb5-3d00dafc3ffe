032673506ac8a61c0d73d6bf2e1c4b3265d3ee7e
dk <kudin.dima@gmail.com>
2026-04-15 18:32:30 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Raleway } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'CogniSphere AI | Next-Gen Intelligence',
+  description: 'CogniSphere AI empowers businesses with cutting-edge machine learning models for scalable automation and predictive insights.',
+  openGraph: {
+    "title": "CogniSphere AI",
+    "description": "Leading AI intelligence for modern enterprises.",
+    "siteName": "CogniSphere AI"
+  },
+};
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${raleway.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
